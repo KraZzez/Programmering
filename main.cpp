237497451b32952836e3b338a28266f0e30aa3c3
KraZzez <ludvig.ludde23@gmail.com>
2018-08-30 13:54:45 +0200

    First try

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Kod på två nya rader" << endl;
+	cout << "rad";
+	cout << endl;
+	
+	// cout är standard utenhet, skärmen
+	// << är utmatningsoperator
+	
+	cout << "Tjebab";
+	// programmet körs rad efter rad
+	// fungerar allt kommer vi fram
+	// till "return 0;"
+	// heltalet 0 returneras till operativsystemet
+	return 0;
+}
