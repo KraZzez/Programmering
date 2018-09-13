84ec903a5dd98c85870f371ddddc5026076c6dac
KraZzez <ludvig.ludde23@gmail.com>
2018-09-13 14:58:53 +0200

    2018-09-13

---
+++ 2018-09-13/Exempel2.2.cpp
@@ -0,0 +1,14 @@
+// cin
+#include <iostream>
+using namespace std;
+int main()
+{
+	int tal1, tal2;
+	cout << "Talet: ";
+	cin >> tal1;
+	cout << "talet: ";
+	cin>> tal2;
+	cout << "är lika med " << tal1 * tal2 << endl;
+	
+	return 0;
+}
