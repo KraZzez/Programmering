0312aa98f167a9dcc3101cbed0dbb08073888796
KraZzez <ludvig.ludde23@gmail.com>
2018-10-18 14:38:47 +0200

    2018-10-18

---
+++ 2018-10-18/prov.cpp
@@ -0,0 +1,87 @@
+//* Ludvig Krantzén, Uppgift 1
+/*#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Skriv in en liten bokstav: ";
+	char gemen;
+	cin >> gemen;
+	gemen = gemen -32;
+	cout << "Motsvarande VERSAL är:" << gemen << endl;
+	
+	return 0;
+}
+*/
+//* Ludvig Krantzén, Uppgift 2
+/*
+#include <iostream>
+using namespace std;
+int main()
+ {
+	 double Tal1, Tal2, Tal3, medelvardet;
+	 cout << "Mata in tre flyttal: ";
+	 cin >> Tal1;
+	 cin >> Tal2;
+	 cin >> Tal3;
+	 cout << "Tal 1: " << Tal1 << endl;
+	 
+	 cout << "Tal 2: " << Tal2 << endl;
+	 
+	 cout << "Tal 3: " << Tal3 << endl;
+	 medelvardet = (Tal1+Tal2+Tal3)/3;
+	 
+
+	 
+	 cout << "Medelvärdet av värderna blir: " << medelvardet << endl;
+	 
+	 
+	 return 0;
+ }
+ */
+//* Ludvig Krantzén, Uppgift 2
+/* 
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale( LC_ALL,"" );
+	int text;
+	string bokstaver;
+cout << "Mata in din text: ";
+cin >> text;
+cout << "Mata in tecken att leta efter: ";
+string bokstav;
+cin >> bokstav;
+s.substr(start, langd);
+cin  >>
+
+
+	
+	
+	
+	
+	
+	
+	
+		
+	return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int timmar, minuter, sekunder;
+	cout << "Mata in timmar, minuter och sekunder: ";
+	cin >> timmar;
+	cin >> minuter:
+	cin >> sekunder:
+	cout << "Timmar: " << timmar << endl;
+	cout << "Minuter: " << minuter << endl;
+    cout << "Sekunder: " << sekunder << endl;
+    
+    
+    
+return 0;
+}
