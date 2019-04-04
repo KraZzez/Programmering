7e30cd215c95635504e2e8e13c1158907765ca15
KraZzez <ludvig.ludde23@gmail.com>
2019-04-04 14:57:49 +0200

    2019-04-04

--- 3 uppgifter/andra.cpp
+++ 3 uppgifter/andra.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int x;
+
 int main() {
-	double falt[]
+	double falt[100];
+	cin >> x;
+	for (int n = 0; n < 100; n++);
+	{
+		cin >> falt (n);
+		if (x == n) {
+		for (int n = n+1; n<100; n++); {
+			falt(n) = 0; }
+		
+			break;}
+	}
+
+		
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 return 0;
 }
